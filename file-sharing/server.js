c635bd4be4ab444855dfabfb15b3e70ca6a70459
Sahil Saini <sahilsaini1969@gmail.com>
2022-07-25 22:36:39 +0530

    initialize file-sharing

---
+++ file-sharing/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const multer = require("multer");
+
+const upload = multer({ dest: 'uploads' });
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.post("/upload", upload.single("file"), (req, res) => {
+    res.send("hi");
+});
+
+
+app.listen(3000, () => console.log("server is running"));
+
